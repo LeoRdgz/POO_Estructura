2fe73be135f8dd88f62b21d5c417420fee3c980c
LeoRdgz <128827842+LeoRdgz@users.noreply.github.com>
2023-05-19 09:36:55 -0500

    Add files via upload

---
+++ POOEstructuras2/Program.cs
@@ -0,0 +1,29 @@
+﻿using POO_Estructura23BM.Entities;
+using POO_Estructura23BM.Servicios;
+using System;
+
+namespace POO_Estructura23BM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AlumnoServicio alumnoServicio = new AlumnoServicio();
+
+            // Alumno alumno = alumnoServicio.Agregar();
+            //var res = alumnoServicio.Agregar();
+            Alumno request = alumnoServicio.Agregar();
+
+            //if (alumnoServicio.ValidarPromedio(request.Promedio))
+            //{
+            //    Console.WriteLine("Calificación aprobada");
+            //    alumnoServicio.Imprimir(request);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No aprobado");
+            //}
+
+        }
+    }
+}
---
+++ POOEstructuras2/Servicios/AlumnoServicios.cs
@@ -0,0 +1,64 @@
+﻿using POO_Estructura23BM.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Estructura23BM.Servicios
+{
+    public class AlumnoServicio
+    {
+
+        // public Alumno Agregar()
+        // ----- Response Request
+        public Alumno Agregar()
+        {
+            try
+            {
+                // Constructor
+                Alumno alumno2 = new Alumno()
+                {
+                    ID = 1,
+                    Nombre = "abcde",
+                    Apellido = "xyz",
+                    Fecha = DateTime.Now,
+                    Grado = 3,
+                    Promedio = 5.2
+                };
+                return alumno2;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(" < Sucedio un error > " + ex.Message);
+            }
+        }
+
+        public void Imprimir(Alumno request)
+        {
+            try
+            {
+                Console.WriteLine("ID: " + request.ID);
+                Console.WriteLine("Nombre: " + request.Nombre);
+                Console.WriteLine("Apellido: " + request.Apellido);
+                Console.WriteLine("Fecha de inscripción: " + request.Fecha);
+                Console.WriteLine("Grado: " + request.Grado);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(" < Sucedio un error > " + ex.Message);
+            }
+        }
+        #region ---Validar---
+        public bool ValidarPromedio(double num)
+        {
+            bool validar = false;
+            if (num >= 7)
+            {
+                validar = true;
+            }
+            return validar;
+        }
+        #endregion
+    }
+}
